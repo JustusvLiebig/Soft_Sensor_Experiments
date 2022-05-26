bec16b873c25e0d5d57fd9924928854f095ea5d3
tonyzyl <tonyzhuangyl@outlook.com>
2022-05-26 10:21:05 +0100

    Update SAEV0.2.py
    Account the unlabeled data, modify the loss func to what proposed in the paper

--- StackedAutoEncoder/SAEV0.2.py
+++ StackedAutoEncoder/SAEV0.2.py
@@ -1,29 +1,38 @@
 import numpy as np
 import pandas as pd
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
-from sklearn.base import BaseEstimator, RegressorMixin
-from sklearn.metrics import mean_squared_error, r2_score
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+import torchvision
+from torchvision import datasets
+import copy
+import pickle
 
-"""
-质量监督堆叠自编码器的代码
-"""
-# 数据集定义方式
+import pandas as pd
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
+
+def seed_everything(seed: int):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        
 class MyDataset(Dataset):
-
     # Initialization
-    def __init__(self, data, label, mode='2D'):
-        self.data, self.label, self.mode = data, label, mode
+    def __init__(self, data, label, unlabel_idx, mode='2D'):
+        self.data, self.label, self.mode, self.unlabel_idx= data, label, mode, unlabel_idx
 
     # Get item
     def __getitem__(self, index):
         if self.mode == '2D':
-            return self.data[index, :], self.label[index, :]
+            return self.data[index, :], self.label[index, :], self.unlabel_idx[index, :]
         elif self.mode == '3D':
-            return self.data[:, index, :], self.label[:, index, :]
+            return self.data[:, index, :], self.label[:, index, :], self.unlabel_idx[:, index, :]
 
     # Get length
     def __len__(self):
@@ -32,174 +41,187 @@ class MyDataset(Dataset):
         elif self.mode == '3D':
             return self.data.shape[1]
 
-# 自编码器的定义
-class AutoEncoder(nn.Module):
-    def __init__(self, dim_X, dim_H):
-        super(AutoEncoder, self).__init__()
-        self.dim_X = dim_X
-        self.dim_H = dim_H
-        self.act = torch.sigmoid
+    def getNumpyLabel(self):
+        return self.label.cpu().detach().numpy()
+    def getTensorTrain(self):
+        return self.data
 
-        self.encoder = nn.Linear(dim_X, dim_H, bias=True)
-        self.decoder = nn.Linear(dim_H, dim_X, bias=True)
+class AE(nn.Module):
+    def __init__(self, input_dim, latent_dim):
+        super().__init__()
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+        self.encoder = nn.Linear(input_dim, latent_dim, bias=True)
+        self.decoder = nn.Linear(latent_dim, input_dim, bias=True)
+        self.y_layer = nn.Linear(latent_dim, 1, bias=True)
 
-    def forward(self, X, rep=False):
-
-        H = self.act(self.encoder(X))
-        if rep is False:
-            return self.act(self.decoder(H))
+    def forward(self, X, decode=True):
+        # Return (latent/decode output, Y estimate by dense)
+        H = torch.sigmoid(self.encoder(X))
+        if decode:
+            return torch.sigmoid(self.decoder(H)), torch.sigmoid(self.y_layer(H))
         else:
-            return H
+            return H, torch.sigmoid(self.y_layer(H))
 
-# 堆叠自编码器定义生成
-class StackedAutoEncoder(nn.Module):
-    def __init__(self, size, device=torch.device('cuda:0')):
-        super(StackedAutoEncoder, self).__init__()
-        self.AElength = len(size)
-        self.SAE = []
-        self.device = device
+class SAE(nn.Module):
+    def __init__(self,AE_list):
+        super().__init__()
+        self.num_AE = len(AE_list)
+        self.SAE_list=[]
 
-        for i in range(1, self.AElength):
-            self.SAE.append(AutoEncoder(size[i-1], size[i]).to(device))
-
-        self.proj = nn.Linear(size[self.AElength-1], 1)
-
-    def forward(self, X, NoL, PreTrain=False):
-        """
-        :param X: 进口参数
-        :param NoL: 第几层
-        :param PreTrain: 是不是无监督预训练
-        :return:
-        """
-        out = X
-        if PreTrain is True:
-            # SAE的预训练
-            if NoL == 0:
-                return out, self.SAE[NoL](out)
+        for i in range(1, self.num_AE+1):
+            if i != self.num_AE:
+                self.SAE_list.append(AE(AE_list[i-1], AE_list[i]).to(device))
+            else:
+                self.SAE_list.append(AE(AE_list[-1], AE_list[-1]).to(device))
+    
+    def wgtFromList(self, wts_list):
+        for i in range(self.num_AE):
+            self.SAE_list[i].load_state_dict(wts_list[i])
+    
+    def forward(self, x, layer_idx, preTrain = False):
+        # preTrain: previous layers' parameters are frozen 
+        # preTrain -> Return (input, AE_output, y estimate)
+        # !preTrain -> Return last layer's (latent, estimate)
+        output = x
+        if preTrain:
+            if layer_idx == 0:
+                inputs = output
+                output, y_estimate = self.SAE_list[layer_idx](output, decode=True)                
+                return inputs, output, y_estimate
 
             else:
-                for i in range(NoL):
-                    # 第N层之前的参数给冻住
-                    for param in self.SAE[i].parameters():
+                for i in range(layer_idx):
+                    for param in self.SAE_list[i].parameters():
                         param.requires_grad = False
-
-                    out = self.SAE[i](out, rep=True)
-                # 训练第N层
-                inputs = out
-                out = self.SAE[NoL](out)
-                return inputs, out
+                    output,_ = self.SAE_list[i](output, decode = False)
+                inputs = output
+                output, y_estimate = self.SAE_list[layer_idx](output, decode=True)                
+                return inputs, output, y_estimate
         else:
-            for i in range(self.AElength-1):
-                # 做微调
-                for param in self.SAE[i].parameters():
+            for i in range(self.num_AE-1):
+                for param in self.SAE_list[i].parameters():
                     param.requires_grad = True
+                output, _ = self.SAE_list[i](output, decode = False)
+            return self.SAE_list[-1](output, decode = False)
 
-                out = self.SAE[i](out, rep=True)
-            out = torch.sigmoid(self.proj(out))
-            return out
+def loss_func(input_encode, output_latent, y_estimate,  y_label, unlabel_idx, tradeoff_param):
+    N_total = unlabel_idx.size(dim=0)
+    N_u = torch.sum(unlabel_idx)
+    N_l = N_total - N_u
+    label_idx = torch.ones(unlabel_idx.size(), dtype=int, device=device)-unlabel_idx
+    if N_u != 0:
+        unlabel_term = (N_total/(2*N_u))*nn.functional.mse_loss(unlabel_idx*input_encode, unlabel_idx*output_latent)
+    else:
+        unlabel_term = 0
+    if N_l != 0:
+        label_term = (N_total/(2*N_l))*(nn.functional.mse_loss(label_idx*input_encode, label_idx*output_latent)+
+                    tradeoff_param*nn.functional.mse_loss(label_idx*y_estimate, label_idx*y_label))
+    else:
+        label_term = 0
+    return unlabel_term+label_term
+                                
 
-# 单层自编码器训练函数
-def trainAE(model, trainloader, epochs, trainlayer, lr):
+def SAE_train(dataloader_dict, SAE_model, unsp_epochs, unsp_lr, tradeoff_param, sp_epochs, sp_lr):
+    SAE_model.to(device)
+    for layer_idx in range(SAE_model.num_AE):
+        print('Pre-training on the {} layer'.format(layer_idx))
+        # Pretraining of each layer
+        optimizer = torch.optim.Adam(SAE_model.SAE_list[layer_idx].parameters(), lr = unsp_lr)
+        best_layer_wts = copy.deepcopy(SAE_model.SAE_list[layer_idx].state_dict())
+        best_loss = np.inf
 
-    optimizer = torch.optim.Adam(model.SAE[trainlayer].parameters(), lr=lr)
-    loss_func = nn.MSELoss()
+        for epoch in range(unsp_epochs):
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    SAE_model.SAE_list[layer_idx].train()
+                else:
+                    SAE_model.SAE_list[layer_idx].eval()
+                
+                for inputs, labels, unlabel_idx in dataloader_dict[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+                    unlabel_idx = unlabel_idx.to(device)
+                    optimizer.zero_grad()
+                    input_encode,output_latent,y_estimate = SAE_model(inputs,layer_idx,preTrain=True)
+                    loss = loss_func(input_encode, output_latent, y_estimate, labels, unlabel_idx, tradeoff_param)
 
-    for j in range(epochs):
-        sum_loss = 0
-        for X, y in trainloader:
-            Hidden, Hidden_reconst = model(X, trainlayer, PreTrain=True)
-            loss = loss_func(Hidden, Hidden_reconst)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            sum_loss += loss.detach().item()
-        print('无监督预训练第{}层的第{}个epoch,'.format(trainlayer+1, j + 1), ',其Loss的大小是:{}'.format(loss.data.cpu().numpy()))
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                loss= loss.item()
+                if phase == 'train':
+                    train_loss = loss
+                else:
+                    val_loss = loss
+            print(f"Epoch {epoch+1}/{unsp_epochs}, train_loss: {train_loss:>7f}, val_loss: {val_loss:>7f}")
+            if phase == 'val' and val_loss < best_loss:
+                best_loss = val_loss
+                best_layer_wts = copy.deepcopy(SAE_model.SAE_list[layer_idx].state_dict())
+        SAE_model.SAE_list[layer_idx].load_state_dict(best_layer_wts)
+        print(f'Best loss: {best_loss:>7f}')
 
-    return model
+    print('Start Finetuning\n')
+    param_list=[]
+    best_wts_list=[]
+    for tmp_sub_model in SAE_model.SAE_list:
+        best_wts_list.append(tmp_sub_model.state_dict())
+        for tmp_param in tmp_sub_model.parameters():
+            param_list.append(tmp_param)
+    optimizer = torch.optim.Adam(param_list, lr=sp_lr)
+    mse_loss = nn.MSELoss()
+    best_loss = np.inf
 
-# SAE训练的代码模型
-class SAEModel(BaseEstimator, RegressorMixin):
-    def __init__(self, AEList=[13, 10, 7, 5], sup_epoch=300, unsp_epoch=200, unsp_batch_size=64, sp_batch_size=64, sp_lr=0.03,
-                 unsp_lr=0.01, device=torch.device('cuda:0'), seed=1024):
-        super(SAEModel, self).__init__()
-        torch.manual_seed(seed)
+    for epoch in range(sp_epochs):
+        for phase in ['train', 'val']:
+            sum_loss=0
+            if phase == 'train':
+                SAE_model.train()
+            else:
+                SAE_model.eval()
 
-        # 参数分配
-        self.AEList = AEList
-        self.num_AE = len(AEList) - 1
-        self.unsp_epoch = unsp_epoch
-        self.sup_epoch = sup_epoch
-        self.unsp_batch_size = unsp_batch_size
-        self.sp_batch_size = sp_batch_size
-        self.unsp_lr = unsp_lr
-        self.sp_lr = sp_lr
-        self.device = device
-        self.seed = seed
-
-        # SAE模型的创建
-        self.StackedAutoEncoderModel = StackedAutoEncoder(size=AEList, device=device).to(device)
-
-        # 有多少AE就要单独定义多少次SAE
-        self.optimizer = optim.Adam(
-            [
-                {'params': self.StackedAutoEncoderModel.parameters(), 'lr': self.unsp_lr},
-                {'params': self.StackedAutoEncoderModel.SAE[0].parameters(), 'lr': self.sp_lr},
-                {'params': self.StackedAutoEncoderModel.SAE[1].parameters(), 'lr': self.sp_lr},
-                {'params': self.StackedAutoEncoderModel.SAE[2].parameters(), 'lr': self.sp_lr}
-            ])
-
-        self.loss_func = nn.MSELoss()
-
-    # 数据拟合
-    def fit(self, X, y):
-        dataset = MyDataset(torch.tensor(X, dtype=torch.float32, device=self.device),
-                            torch.tensor(y, dtype=torch.float32, device=self.device),
-                            '2D')
-
-        un_trainloader = DataLoader(dataset, batch_size=self.unsp_batch_size, shuffle=True)
-        trainloader = DataLoader(dataset, batch_size=self.sp_batch_size, shuffle=True)
-        print(next(self.StackedAutoEncoderModel.parameters()).is_cuda)
-        self.StackedAutoEncoderModel.train()
-
-        for i in range(self.num_AE):
-            print('自编码器训练第{}层:'.format(i + 1))
-            self.StackedAutoEncoderModel = trainAE(model=self.StackedAutoEncoderModel, trainloader=un_trainloader,
-                                                   epochs=self.unsp_epoch, trainlayer=i, lr=self.unsp_lr)
-            print('自编码器第{}层训练完成!'.format(i + 1))
-
-        Loss = []
-        for i in range(self.sup_epoch):
-            sum_loss = 0
-            for batch_X, batch_y in trainloader:
-                pre = self.StackedAutoEncoderModel(batch_X, i, PreTrain=False)
-                loss = self.loss_func(pre, batch_y)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+            for tmp_inputs, tmp_labels, unlabel_idx in dataloader_dict[phase]:
+                unlabel_idx = unlabel_idx.to(device)
+                if unlabel_idx.size(dim=0) == torch.sum(unlabel_idx):
+                    # All data in the batch is unlabelled, bypass
+                    continue
+                label_idx = torch.ones(unlabel_idx.size(), dtype=int, device=device)-unlabel_idx
+                inputs = torch.index_select(tmp_inputs, 0, torch.argwhere(label_idx)[:,0]).to(device)
+                labels = torch.index_select(tmp_labels, 0, torch.argwhere(label_idx)[:,0]).to(device)
+                optimizer.zero_grad()
+                _, y_estimate = SAE_model(inputs,SAE_model.num_AE-1,preTrain=False)
+                loss = mse_loss(y_estimate, labels)
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
                 sum_loss += loss.detach().item()
-            print('有监督微调第{}轮的Loss是{}'.format(i+1, loss.data.cpu().numpy()))
-            Loss.append(sum_loss)
-        # 绘制损失函数曲线
-        plt.figure()
-        plt.plot(range(len(Loss)), Loss, color='b')
-        plt.show()
+            if phase == 'train':
+                train_loss = sum_loss
+            else:
+                val_loss = sum_loss
+        print(f"Epoch {epoch+1}/{sp_epochs}, train_loss: {train_loss:>7f}, val_loss: {val_loss:>7f}")
+        if phase == 'val' and val_loss < best_loss:
+            best_loss = val_loss
+            for AE_idx in range(SAE_model.num_AE):
+                best_wts_list[AE_idx] = copy.deepcopy(SAE_model.SAE_list[AE_idx].state_dict())
+    SAE_model.wgtFromList(best_wts_list)
+    print('Training complete')
+    #with open('wts_list', 'wb') as fp:
+    #    pickle.dump(best_wts_list, fp)
 
-        return self
+def genUnlabelList(data, label, remain = 0.2, mode = '2D'):
+    if mode == '2D':
+        label_idx = np.arange(0, data.shape[0], int(1/remain), dtype=int)
+        unlabel_idx = np.ones((data.shape[0],1)).astype('int')
+    elif mode == '3D':
+        label_idx = np.arange(0, data.shape[1], int(1/remain), dtype=int)
+        unlabel_idx = np.ones((data.shape[1],1)).astype('int')
+    unlabel_idx[label_idx]=0
+    return unlabel_idx
 
-    # 预测数据
-    def predict(self, X):
-        X = torch.as_tensor(X, dtype=torch.float32, device=self.device)
-        self.StackedAutoEncoderModel.eval()
-        with torch.no_grad():
-            y = self.StackedAutoEncoderModel(X, 0, PreTrain=False).cpu().numpy()
-
-        return y
-
-# 数据读取:
 data = pd.read_csv('Debutanizer_Column_Data.txt', sep='\s+')
 data = data.values
 
-# 数据转化:
 x_temp = data[:, :7]
 y_temp = data[:, 7]
 
@@ -221,43 +243,56 @@ x_new[:, 12] = y_temp[:2390]
 y_new = y_temp[4: 2394]
 y_new = y_new.reshape([-1, 1])
 
-#划分数据集
-
 train_x = x_new[:1000, :]
 train_y = y_new[:1000]
 
-x_validation = x_new[1000:1600, :]
-y_validation = y_new[1000:1600]
+val_x = x_new[1000:1600, :]
+val_y = y_new[1000:1600]
 
 test_x = x_new[1600:2390, :]
 test_y = y_new[1600:2390]
 
-# 开始搞活
-mdl = SAEModel(AEList=[13, 10, 7, 5], sup_epoch=100, unsp_epoch=300, unsp_batch_size=50, sp_batch_size=20, sp_lr=0.03,
-                 unsp_lr=0.01, device=torch.device('cuda:0'), seed=1024).fit(train_x, train_y)
+train_unlabel = genUnlabelList(train_x, train_y, remain = 0.2, mode='2D')
+train_dataset = MyDataset(torch.tensor(train_x, dtype=torch.float32, device=device),
+                          torch.tensor(train_y, dtype=torch.float32, device=device),
+                          torch.tensor(train_unlabel, dtype=torch.int, device=device))
+val_unlabel = genUnlabelList(val_x, val_y, remain = 0.2, mode='2D')
+val_dataset = MyDataset(torch.tensor(val_x, dtype=torch.float32, device=device),
+                          torch.tensor(val_y, dtype=torch.float32, device=device),
+                          torch.tensor(val_unlabel, dtype=torch.int, device=device))
+test_unlabel = genUnlabelList(test_x, test_y, remain = 0.2, mode='2D')
+test_dataset = MyDataset(torch.tensor(test_x, dtype=torch.float32, device=device),
+                          torch.tensor(test_y, dtype=torch.float32, device=device),
+                          torch.tensor(test_unlabel, dtype=torch.int, device=device))
 
-output_train = mdl.predict(train_x)
-output_test = mdl.predict(test_x)
+# Key params
+batch_size = 64
 
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+dataloader_dict ={'train' : train_dataloader, 'val' : val_dataloader}
 
-# 训练集画图
-plt.figure()
-plt.plot(range(len(output_train)), output_train, color='b', label='y_trainpre')
-plt.plot(range(len(output_train)), train_y, color='r', label='y_true')
+myModel = SAE(AE_list=[13,10,7,5])
+SAE_train(dataloader_dict , myModel, 50, 0.01, 1.6, 100, 0.01)
+
+myModel.eval()
+with torch.no_grad():
+    _,pred = myModel(torch.tensor(test_x, dtype=torch.float32, device=device),0)
+
+'''
+loadModel = SAE(AE_list=[13,10,7,5])
+#torch.save(model.state_dict(), PATH)
+#loadModel.load_state_dict(torch.load('model.pth'))
+with open('wts_list', 'rb') as fp:
+    wts_list = pickle.load(fp)
+loadModel.wgtFromList(wts_list)
+loadModel.eval()
+with torch.no_grad():
+    _,pred = loadModel(torch.tensor(test_x, dtype=torch.float32, device=device),0)
+'''
+
+plt.plot(pred.cpu().detach().numpy(), label='Prediction')
+plt.plot(test_y, label='Actual')
+plt.xlabel('Number of records')
+plt.ylabel('y value')
 plt.legend()
-plt.show()
-train_rmse = np.sqrt(mean_squared_error(output_train, train_y))
-train_r2 = r2_score(output_train, train_y)
-print('train_rmse = ' + str(round(train_rmse, 5)))
-print('r2 = ', str(train_r2))
-
-# 测试集画图
-plt.figure()
-plt.plot(range(len(output_test)), output_test, color='b', label='y_testpre')
-plt.plot(range(len(output_test)), test_y, color='r', label='y_true')
-plt.legend()
-plt.show()
-test_rmse = np.sqrt(mean_squared_error(output_test, test_y))
-test_r2 = r2_score(output_test, test_y)
-print('test_rmse = ' + str(round(test_rmse, 5)))
-print('r2 = ', str(test_r2))
